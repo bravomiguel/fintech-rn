cac538aa5e1dc0fadd6d6c2f50cb7ba52bad49cc
Miguel Bravo <miguelbravobalestrini@gmail.com>
2024-09-23 15:27:05 -0500

    info and listings api routes initial set-up

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,7 +1,12 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Page = () => {
+
+  useEffect(() => {
+  
+  }, [])
+  
   return (
     <View>
       <Text>Crypto</Text>
